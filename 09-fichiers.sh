d17c78e5814c79bdc156672b2ebba4f8da384684
NikkiCloud <lockser498jubia@gmail.com>
2023-05-01 11:05:33 -0400

    commande pr fichiers

--- 09-fichiers.sh
+++ 09-fichiers.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+#-e : fichier existe
+# la commande test -e cest lequivalent de [[ -e $1 ]]
+# -f = c'est une fichier?
+# -d = c'est un dossier?
+
+if test -e $1
+then
+	echo "le fichier existe"
+fi
