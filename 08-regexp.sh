eb1dccb2b0155510b6495681b8836b902ec8ec51
NikkiCloud <lockser498jubia@gmail.com>
2023-05-01 10:58:49 -0400

    commande pr expression reg

--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+read -p "Texte : " txt
+
+# expression reguliere qui dit que ca doit commencer par un chiffre de 0-9 et apres ca pas d'importance
+if [[ $txt =~ ^[0-9]+$ ]]
+then
+	echo "Numerique"
+fi
