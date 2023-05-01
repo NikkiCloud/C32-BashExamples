eb0388fc470548c995d7d4566e0f6aead2bf9fe7
NikkiCloud <lockser498jubia@gmail.com>
2023-05-01 11:46:05 -0400

    exercice2 fait

--- exercice2.sh
+++ exercice2.sh
@@ -1,1 +1,26 @@
 #!/bin/bash
+
+# 1- Afficher un menu
+#2 - Mettre le menu dans une fonction
+#3 - Faire une boucle:
+#           Offrir 2 choix: Quitter (choix q),
+#            ou appelex ./exercice1.sh (choix d)
+#4 - À chaque debut de boucle, afficher le menu
+
+monMenu(){
+    echo "q - Quitter"
+    echo "d - Appeler ./exercice1.sh"
+    
+}
+resulat =
+while [[ $resultat != q ]]
+    do
+        monMenu
+        read -p "Votre choix : " resultat
+        if [[ $resultat = d ]]
+        then
+            ./exercice1.sh
+            sleep 1
+        fi
+    done
+
