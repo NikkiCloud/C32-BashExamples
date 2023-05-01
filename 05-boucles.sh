e7a426060367351c2cab8639b7b653cdf531e523
NikkiCloud <lockser498jubia@gmail.com>
2023-05-01 10:43:47 -0400

    commande pr faire boucles

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
 
+for ((i=0;i<5;i++))
+do
+	echo $i
+done
+
+# `` ca va executer la commade ls et le resultat de la commande va dans liste
+liste=`ls`
+
+# une boucle foreach
+for fichier in $liste
+do
+	echo $fichier
+done
+
+resultat=o
+
+while [[ $resultat = o ]]
+do
+	read -p "Choix: " resultat
+done
