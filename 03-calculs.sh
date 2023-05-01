55763540ac6bd2dba3f48e2b13b02137d5b0d789
NikkiCloud <lockser498jubia@gmail.com>
2023-05-01 10:04:03 -0400

    operation mathematique

--- 03-calculs.sh
+++ 03-calculs.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 
+echo 10+10
+
+echo $((10+10))
+# si on veut faire calcul il faire $(())
+
+valeur=$((10+10))
+# on assigne le calcul a la variable valeur
+echo $valeur
+
+#une autre facon de faire ne operation mathematique est de mettre let devaant
+let resultat=10*10
+echo $resultat
