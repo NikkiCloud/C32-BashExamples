288bed95ad1bc4f6c34dd4eb974a80fca71aea35
NikkiCloud <lockser498jubia@gmail.com>
2023-05-01 10:56:10 -0400

    commande pr fonctions

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# $1 va nous donner le prmeier parametre qui est passer... donc en fait ca nous permet de passer des parametres
+maFonction() {
+	echo "param : " $1
+	echo "param : " $2
+}
+
+maFonction "salut" "bonjour"
+maFonction "toi" "moi"
+
+
+
