0c25d07bb4ebaec3f3b0abf630844572cd5a9d4a
NikkiCloud <lockser498jubia@gmail.com>
2023-05-01 12:23:02 -0400

    exercice3 fait

--- exercice3.sh
+++ exercice3.sh
@@ -1,1 +1,49 @@
 #!/bin/bash
+
+
+# affichageArobas(){
+#     echo -n "@"
+# }
+
+# arobasQuiBouge(){
+#     clear
+#     echo -n " "
+#     affichageArobas
+#     sleep 2
+#     echo -n " "
+# }
+
+
+# for ((compteur = 0;$compteur<5;compteur++))
+# do
+#     arobasQuiBouge
+# done
+
+faireEspace(){
+    #1 = nombre d'espaces à faire
+
+    #Boucle qui fait des espaces sur la meme ligne
+
+    for ((i=0;i < $1;i++))
+    do 
+        echo -n " "
+    done
+}
+
+for ((i=0;i<10;i++))
+do
+    clear
+    faireEspace $i
+
+    if [[ $i -lt 9 ]]
+    then
+        echo "@"
+    else
+        echo "BOOM"
+    fi
+
+    sleep 0.1
+done
+
+
+
